c00c7443d82a803c0f23a1699aa8d65b2c0c9713
Prabhu Kuttiyam <pkuttiyam@ti.com>
2012-03-12 18:54:23 -0400

    hwqueue: keystone: add keystone hwqueue driver
    This commit adds an hwqueue driver for the keystone qmss hardware.  As of this
    commit, accumulator functionality is non-existent.
    
    Signed-off-by: Prabhu Kuttiyam <pkuttiyam@ti.com>
    Signed-off-by: Cyril Chemparathy <cyril@ti.com>
    
    hwqueue: keystone: avoid non-cacheable coherent allocs for descriptor regions
    
    This patch replaces the original dma coherent allocation with a cacheable
    allocation that is then mapped for dma usage.
    
    hwqueue: keystone: add optimization hints
    
    In addition to a few cosmetic fixes, this patch mainly adds branch
    optimization hints for the compiler's benefit.  We also prefetch descriptors
    on pop, and mournfully retry on finding an invalid descriptor on a hardware
    queue.
    
    hwqueue: cleanup unused keystone specific structures
    
    The hwqueue header had unnecessary remnant definitions from the
    pre-device-tree era.  These have been removed by this patch.
    
    hwqueue: align descriptor size to cache line size
    
    This patch aligns descriptor size to the cache line size at push.
    
    hwqueue: keystone: verify descriptor size on push
    
    This patch adds verification on descriptor size before pushing it to a
    hardware queue.
    
    hwqueue: keystone: scatter/gather support
    
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>
    
    hwqueue: keystone: add support for multiple queue managers
    
    On the next generation of keystone devices, there will be multiple
    instances of the queue manager. This commit adds support for
    multiple queue managers in the keystone hardware queue driver.
    On the current range of keystone devices we have only one queue
    manager. Thus we will have only one queue manager node in the
    device tree bindings.
    
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>
    
    hwqueue: setting up pools to explicitly choose region numbers
    
    Signed-off-by: Prabhu Kuttiyam <pkuttiyam@ti.com>
    
    hwqueue: keystone: add range ops support in core driver

---
+++ drivers/hwqueue/keystone_hwqueue.h
@@ -0,0 +1,177 @@
+/*
+ * Keystone hardware queue driver
+ *
+ * Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com
+ * Contact: Prabhu Kuttiyam <pkuttiyam@ti.com>
+ *	    Cyril Chemparathy <cyril@ti.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef __KEYSTONE_HWQUEUE_H__
+#define __KEYSTONE_HWQUEUE_H__
+
+#define DESC_SIZE_MASK	0xful
+#define DESC_PTR_MASK	(~DESC_SIZE_MASK)
+
+#define THRESH_GTE	BIT(7)
+#define THRESH_LT	0
+
+struct khwq_reg_config {
+	u32		revision;
+	u32		__pad1;
+	u32		divert;
+	u32		link_ram_base0;
+	u32		link_ram_size0;
+	u32		link_ram_base1;
+	u32		__pad2[2];
+	u32		starvation[0];
+};
+
+struct khwq_reg_region {
+	u32		base;
+	u32		start_index;
+	u32		size_count;
+	u32		__pad;
+};
+
+struct khwq_reg_queue {
+	u32		entry_count;
+	u32		byte_count;
+	u32		packet_size;
+	u32		ptr_size_thresh;
+};
+
+struct khwq_region {
+	const char	*name;
+	unsigned	 used_desc;
+	struct list_head list;
+	unsigned	 id;
+	unsigned	 desc_size;
+	unsigned	 num_desc;
+	unsigned	 fixed_mem;
+	dma_addr_t	 next_pool_addr;
+	dma_addr_t	 dma_start, dma_end;
+	void		*virt_start, *virt_end;
+	unsigned	 link_index;
+};
+
+struct khwq_pool_info {
+	const char		*name;
+	struct khwq_region	*region;
+	int			 region_offset;
+	int			 num_desc;
+	int			 desc_size;
+	int			 region_id;
+	struct hwqueue		*queue;
+	struct list_head	 list;
+};
+
+struct khwq_link_ram_block {
+	dma_addr_t	 phys;
+	void		*virt;
+	size_t		 size;
+};
+
+struct khwq_range_ops;
+
+struct khwq_range_info {
+	const char		*name;
+	struct khwq_device	*kdev;
+	unsigned		 queue_base;
+	unsigned		 num_queues;
+	unsigned		 irq_base;
+	unsigned		 flags;
+	struct list_head	 list;
+	struct khwq_range_ops	*ops;
+};
+
+struct khwq_range_ops {
+	int	(*init_range)(struct khwq_range_info *range);
+	int	(*free_range)(struct khwq_range_info *range);
+	int	(*init_queue)(struct khwq_range_info *range,
+			      struct hwqueue_instance *inst);
+	int	(*open_queue)(struct khwq_range_info *range,
+			      struct hwqueue_instance *inst, unsigned flags);
+	int	(*close_queue)(struct khwq_range_info *range,
+			       struct hwqueue_instance *inst);
+	int	(*set_notify)(struct khwq_range_info *range,
+			      struct hwqueue_instance *inst, bool enabled);
+};
+
+#define RANGE_RESERVED		BIT(0)
+#define RANGE_HAS_IRQ		BIT(1)
+
+struct khwq_qmgr_info {
+	unsigned			 start_queue;
+	unsigned			 num_queues;
+	struct khwq_reg_config __iomem	*reg_config;
+	struct khwq_reg_region __iomem	*reg_region;
+	struct khwq_reg_queue __iomem	*reg_push, *reg_pop, *reg_peek;
+	void __iomem			*reg_status;
+	struct list_head		 list;
+};
+
+struct khwq_device {
+	struct device			*dev;
+	struct hwqueue_device		 hdev;
+	struct list_head		 regions;
+	struct khwq_link_ram_block	 link_rams[2];
+	unsigned			 num_queues;
+	unsigned			 base_id;
+	struct list_head		 queue_ranges;
+	struct list_head		 pools;
+	struct list_head		 qmgrs;
+};
+
+struct khwq_instance {
+	struct khwq_device	*kdev;
+	struct khwq_range_info	*range;
+	struct khwq_region	*last; /* cache last region used */
+	struct khwq_qmgr_info	*qmgr; /* cache qmgr for the instance */
+	int			 irq_num; /*irq num -ve for non-irq queues */
+	char			 irq_name[32];
+};
+
+#define to_hdev(_kdev)		(&(_kdev)->hdev)
+#define from_hdev(_hdev)	container_of(_hdev, struct khwq_device, hdev)
+
+#define for_each_region(kdev, region)				\
+	list_for_each_entry(region, &kdev->regions, list)
+
+#define for_each_queue_range(kdev, range)			\
+	list_for_each_entry(range, &kdev->queue_ranges, list)
+
+#define first_queue_range(kdev)					\
+	list_first_entry(&kdev->queue_ranges, struct khwq_range_info, list)
+
+#define for_each_pool(kdev, pool)				\
+	list_for_each_entry(pool, &kdev->pools, list)
+
+#define for_each_qmgr(kdev, qmgr)				\
+	list_for_each_entry(qmgr, &kdev->qmgrs, list)
+
+static inline struct khwq_qmgr_info *
+khwq_find_qmgr(struct hwqueue_instance *inst)
+{
+	struct khwq_device *kdev = from_hdev(inst->hdev);
+	unsigned id = hwqueue_inst_to_id(inst);
+	struct khwq_qmgr_info *qmgr;
+
+	for_each_qmgr(kdev, qmgr) {
+		if ((id >= qmgr->start_queue) &&
+			(id < qmgr->start_queue + qmgr->num_queues))
+			return qmgr;
+	}
+
+	return NULL;
+}
+
+#endif /* __KEYSTONE_HWQUEUE_H__ */
