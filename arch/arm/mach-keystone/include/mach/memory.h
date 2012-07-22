5e1cd863adf1b5e6be834dd324d5fb82828e6358
Cyril Chemparathy <cyril@ti.com>
2012-07-22 08:31:48 -0400

    ARM: keystone: introducing TI Keystone platform
    Texas Instruments Keystone family of multicore devices now includes an
    upcoming slew of Cortex A15 based devices.  This patch adds basic definitions
    for a new Keystone sub-architecture in ARM.
    
    Subsequent patches in this series will extend support to include SMP and take
    advantage of the large physical memory addressing capabilities via LPAE.
    
    Signed-off-by: Vitaly Andrianov <vitalya@ti.com>
    Signed-off-by: Cyril Chemparathy <cyril@ti.com>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>

---
+++ arch/arm/mach-keystone/include/mach/memory.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2010-2012 Texas Instruments, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __ASM_MACH_MEMORY_H
+#define __ASM_MACH_MEMORY_H
+
+#define MAX_PHYSMEM_BITS	36
+#define SECTION_SIZE_BITS	34
+
+#endif /* __ASM_MACH_MEMORY_H */
