37c2dce261be82455498b0dc55df2c53afe12d1b
Cyril Chemparathy <cyril@ti.com>
2012-07-22 09:54:19 -0400

    ARM: keystone: add switch over to high physical address range
    Keystone platforms have their physical memory mapped at an address outside the
    32-bit physical range.  A Keystone machine with 16G of RAM would find its
    memory at 0x0800000000 - 0x0bffffffff.
    
    For boot purposes, the interconnect supports a limited alias of some of this
    memory within the 32-bit addressable space (0x80000000 - 0xffffffff).  This
    aliasing is implemented in hardware, and is not intended to be used much
    beyond boot.  For instance, DMA coherence does not work when running out of
    this aliased address space.
    
    Therefore, we've taken the approach of booting out of the low physical address
    range, and subsequently we switch over to the high range once we're safely
    inside machine specific territory.  This patch implements this switch over
    mechanism, which involves rewiring the TTBRs and page tables to point to the
    new physical address space.
    
    Signed-off-by: Vitaly Andrianov <vitalya@ti.com>
    Signed-off-by: Cyril Chemparathy <cyril@ti.com>

--- arch/arm/mach-keystone/include/mach/memory.h
+++ arch/arm/mach-keystone/include/mach/memory.h
@@ -19,4 +19,29 @@
 #define MAX_PHYSMEM_BITS	36
 #define SECTION_SIZE_BITS	34
 
+#define KEYSTONE_LOW_PHYS_START		0x80000000ULL
+#define KEYSTONE_LOW_PHYS_SIZE		0x80000000ULL /* 2G */
+#define KEYSTONE_LOW_PHYS_END		(KEYSTONE_LOW_PHYS_START + \
+					 KEYSTONE_LOW_PHYS_SIZE - 1)
+
+#define KEYSTONE_HIGH_PHYS_START	0x800000000ULL
+#define KEYSTONE_HIGH_PHYS_SIZE		0x400000000ULL	/* 16G */
+#define KEYSTONE_HIGH_PHYS_END		(KEYSTONE_HIGH_PHYS_START + \
+					 KEYSTONE_HIGH_PHYS_SIZE - 1)
+#ifdef CONFIG_ARM_LPAE
+
+#ifndef __ASSEMBLY__
+
+static inline phys_addr_t __virt_to_idmap(unsigned long x)
+{
+	return (phys_addr_t)(x) - CONFIG_PAGE_OFFSET +
+		KEYSTONE_LOW_PHYS_START;
+}
+
+#define virt_to_idmap(x)	__virt_to_idmap((unsigned long)(x))
+
+#endif /* __ASSEMBLY__ */
+
+#endif /* CONFIG_ARM_LPAE */
+
 #endif /* __ASM_MACH_MEMORY_H */
