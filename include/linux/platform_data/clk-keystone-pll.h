952b4fbc3c8e5e5882acb7b7d88fdc3cda55d36b
Murali Karicheri <m-karicheri2@ti.com>
2012-09-17 20:30:04 -0400

    clk: keystone - add Main PLL clock driver
    This is the driver for the main PLL clock hardware found on Keystone
    devices (c6x such as C6678). This driver is implemented as per common
    clock provider API. The main PLL hardware typically has a multiplier,
    and a divider. struct clk_keystone_pll_data is used to configure the
    driver for a specific platform.
    
    Signed-off-by: Murali Karicheri <m-karicheri2@ti.com>

---
+++ include/linux/platform_data/clk-keystone-pll.h
@@ -0,0 +1,37 @@
+/*
+ * TI Keyston clk-pll driver platform data definitions
+ *
+ * Copyright (C) 2012 Texas Instruments.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef __CLK_KEYSTONE_PLL_H
+#define __CLK_KEYSTONE_PLL_H
+
+struct clk_keystone_pll_data {
+	/* register holds lower bits of PLLM */
+	u32 phy_pllm;
+	/* holds upper bits of PLLM */
+	u32 phy_main_pll_ctl0;
+	/* mapped addresses. should be initialized by  */
+	void __iomem *pllm;
+	void __iomem *main_pll_ctl0;
+	u32 pllm_lower_mask;
+	u32 pllm_upper_mask;
+	u32 pllm_upper_shift;
+	u32 plld_mask;
+	/* use this value for postdiv */
+	u32 fixed_postdiv;
+};
+
+extern struct clk *clk_register_keystone_pll(struct device *dev,
+			const char *name, const char *parent_name,
+			struct clk_keystone_pll_data *pll_data);
+#endif /* CLK_KEYSTONE_PLL_H */
