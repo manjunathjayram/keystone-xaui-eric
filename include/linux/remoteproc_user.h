e611667200005bcfa7ea6ad1079382b73476e7a0
Cyril Chemparathy <cyril@ti.com>
2012-12-10 11:20:50 -0500

    remoteproc: add support for user-space loaders
    Initial commit - more details to follow soon
    
    remoteproc: rproc-user: honor names

---
+++ include/linux/remoteproc_user.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2012 Texas Instruments Incorporated
+ * Author: Cyril Chemparathy <cyril@ti.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _LINUX_REMOTEPROC_USER_H
+#define _LINUX_REMOTEPROC_USER_H
+
+#include <linux/types.h>
+
+enum rproc_user_state {
+	RPROC_USER_OFFLINE,
+	RPROC_USER_RUNNING,
+};
+
+#define RPROC_USER_IOC_MAGIC	 'I'
+
+#define RPROC_USER_IOC_SET_RSC	 _IOW(RPROC_USER_IOC_MAGIC, 0,	\
+				      void *)
+#define RPROC_USER_IOC_SET_STATE _IOW(RPROC_USER_IOC_MAGIC, 1,	\
+				      enum rproc_user_state)
+
+#endif /* _LINUX_REMOTEPROC_USER_H */
