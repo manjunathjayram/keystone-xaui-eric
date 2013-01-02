43005eb4614c751bdb3d229cc1732da80a5c3cca
Murali Karicheri <m-karicheri2@ti.com>
2013-01-02 16:26:12 -0500

    usb: dwc3: force host mode
    hack alert! this must be made configurable

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -484,6 +484,7 @@ static int dwc3_probe(struct platform_device *pdev)
 		}
 		break;
 	case DWC3_MODE_HOST:
+	case DWC3_MODE_DRD:
 		dwc3_set_mode(dwc, DWC3_GCTL_PRTCAP_HOST);
 		ret = dwc3_host_init(dwc);
 		if (ret) {
@@ -491,6 +492,7 @@ static int dwc3_probe(struct platform_device *pdev)
 			goto err1;
 		}
 		break;
+#if 0
 	case DWC3_MODE_DRD:
 		dwc3_set_mode(dwc, DWC3_GCTL_PRTCAP_OTG);
 		ret = dwc3_host_init(dwc);
@@ -505,6 +507,7 @@ static int dwc3_probe(struct platform_device *pdev)
 			goto err1;
 		}
 		break;
+#endif
 	default:
 		dev_err(dev, "Unsupported mode of operation %d\n", mode);
 		goto err1;
