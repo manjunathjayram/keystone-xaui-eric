e3e046a98ea8cbcf5b7027ef8d194a1d38253776
Murali Karicheri <m-karicheri2@ti.com>
2013-01-03 14:49:56 -0500

    clk: keystone: enhancement to support ARMPLL, DDRPLL and PAPLL
    The clk driver for Main PLL is enhanced to support additional PLLs
    found on keystone2 devices. The addtional PLLs are controlled by
    the register where as the Main PLL is controlled by a PLL
    controller. Add a has_pll_cntrl boolean to indicate if the PLL
    is controlled by a pllctrl or not. Some updates made to make code
    more generic.
    
    Signed-off-by: Murali Karicheri <m-karicheri2@ti.com>
    Signed-off-by: Sandeep Nair <sandeep_n@ti.com>

--- drivers/clk/keystone/clk-keystone-pll.c
+++ drivers/clk/keystone/clk-keystone-pll.c
@@ -37,25 +37,35 @@ static unsigned long clk_pllclk_recalc(struct clk_hw *hw,
 	struct clk_pll *pll = to_clk_pll(hw);
 	struct clk_keystone_pll_data *pll_data = pll->pll_data;
 	unsigned long rate = parent_rate;
-	u32  pllm, plld, postdiv, val;
+	u32  mult = 0, prediv, postdiv, val;
 
-	/* get bit0-5 of PLLM from PLLM PLL control register */
-	val = __raw_readl(pll_data->pllm);
-	pllm = (val & pll_data->pllm_lower_mask);
+	/*
+	 * get bits 0-5 of multiplier from pllctrl PLLM register
+	 * if has_pllctrl is non zero */
+	if (pll_data->has_pllctrl) {
+		val = __raw_readl(pll_data->pllm);
+		mult = (val & pll_data->pllm_lower_mask);
+	}
 
 	/* bit6-12 of PLLM is in Main PLL control register */
-	val = __raw_readl(pll_data->main_pll_ctl0);
-	pllm |= ((val & pll_data->pllm_upper_mask)
+	val = __raw_readl(pll_data->pll_ctl0);
+	mult |= ((val & pll_data->pllm_upper_mask)
 			>> pll_data->pllm_upper_shift);
-	plld = (val & pll_data->plld_mask);
+	prediv = (val & pll_data->plld_mask);
 	postdiv = pll_data->fixed_postdiv;
 
-	rate /= (plld + 1);
-	rate = (rate * (pllm + 1));
+	rate /= (prediv + 1);
+	rate = (rate * (mult + 1));
 	rate /= postdiv;
 
-	pr_notice("main_pll_clk rate is %ld, postdiv = %d, pllm = %d," \
-		"plld = %d\n", rate, postdiv, pllm, plld);
+	if (pll_data->has_pllctrl) {
+		pr_notice("main_pll_clk rate is %ld, postdiv = %d, mult = %d," \
+				"prediv = %d\n", rate, postdiv, mult, prediv);
+	} else {
+		pr_notice("pll_clk parent_rate(%ld Hz), rate(%ld Hz)," \
+				"postdiv = %d, mult = %d, prediv = %d\n",
+				parent_rate, rate, postdiv, mult, prediv);
+	}
 	return rate;
 }
 
@@ -104,24 +114,38 @@ void __init of_keystone_pll_clk_init(struct device_node *node)
 	struct clk_keystone_pll_data *pll_data;
 	const char *parent_name;
 	struct clk *clk;
+	int temp;
 
 	pll_data = kzalloc(sizeof(*pll_data), GFP_KERNEL);
 	WARN_ON(!pll_data);
 
 	parent_name = of_clk_get_parent_name(node, 0);
 
-	pll_data->pllm = of_iomap(node, 0);
-	WARN_ON(!pll_data->pllm);
+	if (of_find_property(node, "pll_has_pllctrl", &temp)) {
+		/* PLL is controlled by the pllctrl */
+		pll_data->has_pllctrl = 1;
+		pll_data->pllm = of_iomap(node, 0);
+		WARN_ON(!pll_data->pllm);
 
-	pll_data->main_pll_ctl0 = of_iomap(node, 1);
-	WARN_ON(!pll_data->main_pll_ctl0);
+		pll_data->pll_ctl0 = of_iomap(node, 1);
+		WARN_ON(!pll_data->pll_ctl0);
 
-	if (of_property_read_u32(node, "pllm_lower_mask",
+		if (of_property_read_u32(node, "pllm_lower_mask",
 			&pll_data->pllm_lower_mask))
-		goto out;
+			goto out;
+
+	} else {
+		/* PLL is controlled by the ctrl register */
+		pll_data->has_pllctrl = 0;
+		pll_data->pll_ctl0 = of_iomap(node, 0);
+	}
 
 	if (of_property_read_u32(node, "pllm_upper_mask",
 			&pll_data->pllm_upper_mask))
+		goto out;
+
+	if (of_property_read_u32(node, "pllm_upper_shift",
+			&pll_data->pllm_upper_shift))
 		goto out;
 
 	if (of_property_read_u32(node, "plld_mask", &pll_data->plld_mask))
