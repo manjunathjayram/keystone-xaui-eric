e4b86d6128d9984a659f161d6daf59f3a9ba3fd8
Reece R. Pollack <x0183204@ti.com>
2013-06-12 14:23:02 -0400

    net: keystone: Optimize psdata alignment function
    This patch adds special cases for the most common alignments
    to allow the compiler to generate more-efficient shift operators
    rather than a slower divide/modulo calculation. Since this occurs
    on every packet transmitted through the PA (i.e. ALL of them), this
    optimization is worth the possible reduction in obviousness.
    
    Signed-off-by: Reece R. Pollack <x0183204@ti.com>

--- drivers/net/ethernet/ti/keystone_net_core.c
+++ drivers/net/ethernet/ti/keystone_net_core.c
@@ -448,7 +448,15 @@ int netcp_align_psdata(struct netcp_packet *p_info, unsigned byte_align)
 		padding = -EINVAL;
 		break;
 	case 1:
+	case 2:
+	case 4:
 		padding = 0;
+		break;
+	case 8:
+		padding = (p_info->psdata_len << 2) % 8;
+		break;
+	case 16:
+		padding = (p_info->psdata_len << 2) % 16;
 		break;
 	default:
 		padding = (p_info->psdata_len << 2) % byte_align;
