afc6757b15d55ba0850cae5cd00127ebe0eeae0e
WingMan Kwok <w-kwok2@ti.com>
2013-09-03 16:41:20 -0400

    net: keystone: xge: update phy configurations
    Updated xge phy configurations.  Configurations includes chip level and
    port level configurations.
    
    Signed-off-by: WingMan Kwok <w-kwok2@ti.com>

--- drivers/net/phy/mv88x2242.c
+++ drivers/net/phy/mv88x2242.c
@@ -17,19 +17,74 @@
  */
 
 #include <linux/module.h>
+#include <linux/delay.h>
 #include <linux/phy.h>
 #include <linux/of.h>
+#include <linux/gpio.h>
 
-#define MARVELL_PHY_ID_88X2242	0x01410db0
+#define MARVELL_PHY_ID_88X2242	0x01410f12
+#define MARVELL_PHY_ID_MASK	0xfffffc00
 
-#define MV88X2242_PMD_RX_SIGNAL_DETECT	(MII_ADDR_C45 | 0x1000a)
-#define MV88X2242_10GBASER_PCS_STATUS	(MII_ADDR_C45 | 0x30020)
-#define MV88X2242_XGXS_LANE_STATUS	(MII_ADDR_C45 | 0x40018)
+/* chip level regs */
+#define MV88X2242_TX_SRC_N		(MII_ADDR_C45 | 0x1ff400)
+#define MV88X2242_TX_SRC_M		(MII_ADDR_C45 | 0x1ff401)
+#define MV88X2242_HOST_LANE_MUX		(MII_ADDR_C45 | 0x1ff402)
+#define MV88X2242_HW_RESET		(MII_ADDR_C45 | 0x1ff404)
 
-#define MV88X2242_LASI_CONTROL		(MII_ADDR_C45 | 0x38000)
-#define MV88X2242_LASI_STATUS		(MII_ADDR_C45 | 0x38001)
+/* port level regs */
+#define MV88X2242_PCS_CONFIG		(MII_ADDR_C45 | 0x1ff002)
+#define MV88X2242_PORT_RESET		(MII_ADDR_C45 | 0x1ff003)
+#define MV88X2242_GPIO_TRI_CTRL		(MII_ADDR_C45 | 0x1ff013)
+#define MV88X2242_GPIO_INTR_TYPE3	(MII_ADDR_C45 | 0x1ff016)
+
+/* SFI PMA regs */
+#define MV88X2242_PMD_RX_SIGNAL_DETECT	(MII_ADDR_C45 | 0x01000a)
+
+/* SFI 10BASE-R PCS regs */
+#define MV88X2242_10GBASER_PCS_CTRL	(MII_ADDR_C45 | 0x030000)
+#define MV88X2242_10GBASER_PCS_STATUS	(MII_ADDR_C45 | 0x030020)
+#define MV88X2242_LASI_CONTROL		(MII_ADDR_C45 | 0x038000)
+#define MV88X2242_LASI_STATUS		(MII_ADDR_C45 | 0x038001)
+
+/* XFI 10BASE-R PCS regs */
+#define MV88X2242_BASER_PCS_STATUS	(MII_ADDR_C45 | 0x040020)
+
+/* FIXME */
+static int mv88x2242_chip_cfg;
 
 #if IS_ENABLED(CONFIG_OF_MDIO)
+static int mv88x2242_chip_level_config(struct phy_device *phydev)
+{
+	int val;
+
+	/* chip hw reset */
+	phy_write(phydev, MV88X2242_HW_RESET, 0x4000);
+
+	mdelay(10);
+
+	/* Map ports */
+	/* Added by TI for port mapping 0123:0123,
+	   The default is M0-to-N0, M2-to-N1,
+	   so setting bit 9 makes M1-to-N1 instead.
+	   Must be done prior to the reset!
+	   BEGR: I believe only one write is required for this,
+		 as 31.F4xx registers are accessible through any of the
+		 4 PHY addresses.
+	*/
+	phy_write(phydev, MV88X2242_HOST_LANE_MUX, BIT(9));
+
+	/* Shut down unused lanes */
+	val = phy_read(phydev, MV88X2242_TX_SRC_N);
+	val &= 0x00ff;
+	phy_write(phydev, MV88X2242_TX_SRC_N, val);
+
+	val = phy_read(phydev, MV88X2242_TX_SRC_M);
+	val &= 0x00ff;
+	phy_write(phydev, MV88X2242_TX_SRC_M, val);
+
+	return 0;
+}
+
 /* Set and/or override some configuration registers based on the
  * marvell,c45-reg-init property stored in the of_node for the phydev.
  *
@@ -45,62 +100,107 @@
  */
 static int mv88x2242_of_reg_init(struct phy_device *phydev)
 {
-	const __be32 *paddr;
-	const __be32 *paddr_end;
-	int len, ret;
+	int val;
 
-	if (!phydev->dev.of_node)
+	if (phydev->priv) {
+		dev_info(&phydev->dev, "phy %d already configured\n",
+			phydev->addr);
 		return 0;
-
-	paddr = of_get_property(phydev->dev.of_node,
-				"marvell,c45-reg-init", &len);
-	if (!paddr)
-		return 0;
-
-	paddr_end = paddr + (len /= sizeof(*paddr));
-
-	ret = 0;
-
-	while (paddr + 3 < paddr_end) {
-		u16 devid	= be32_to_cpup(paddr++);
-		u16 reg		= be32_to_cpup(paddr++);
-		u16 mask	= be32_to_cpup(paddr++);
-		u16 val_bits	= be32_to_cpup(paddr++);
-		int val;
-		u32 regnum = MII_ADDR_C45 | (devid << 16) | reg;
-		val = 0;
-		if (mask) {
-			val = phy_read(phydev, regnum);
-			if (val < 0) {
-				ret = val;
-				goto err;
-			}
-			val &= mask;
-		}
-		val |= val_bits;
-
-		ret = phy_write(phydev, regnum, val);
-		if (ret < 0)
-			goto err;
 	}
-err:
-	return ret;
+
+	/*10gx2, 6G */
+	phy_write(phydev, MV88X2242_PCS_CONFIG, 0x7171);
+
+	/* pcs reset */
+	phy_write(phydev, MV88X2242_PORT_RESET, 0x8080);
+
+	mdelay(10);
+
+	/* Required to Enable Optical module transmitter */
+	/* For Fiber Tx Disable to enable optics (REQUIRED) */
+	val = phy_read(phydev, MV88X2242_GPIO_INTR_TYPE3);
+	val = (val & ~0x0018) | 0x0010;
+	phy_write(phydev, MV88X2242_GPIO_INTR_TYPE3, val);
+
+	/* Enable Marvell I2C SCL/SDA operations */
+	val = phy_read(phydev, MV88X2242_GPIO_INTR_TYPE3);
+	val = (val & ~0x0800) | 0x0800;
+	phy_write(phydev, MV88X2242_GPIO_INTR_TYPE3, val);
+
+	val = phy_read(phydev, MV88X2242_GPIO_INTR_TYPE3);
+	val = (val & ~0x8000) | 0x8000;
+	phy_write(phydev, MV88X2242_GPIO_INTR_TYPE3, val);
+
+	val = phy_read(phydev, MV88X2242_GPIO_TRI_CTRL);
+	val = (val & ~0x0800) | 0x0800;
+	phy_write(phydev, MV88X2242_GPIO_TRI_CTRL, val);
+
+	mdelay(10);
+
+	phydev->priv = (void *)1;
+
+	/* Read Each Lane Optical traceiver name? */
+
+	return 0;
 }
 #else
+static int mv88x2242_chip_level_config(struct phy_device *phydev)
+{
+	return 0;
+}
+
 static int mv88x2242_of_reg_init(struct phy_device *phydev)
 {
 	return 0;
 }
 #endif /* CONFIG_OF_MDIO */
 
+static int mv88x2242_gpiochip_match_by_label(struct gpio_chip *chip, void *data)
+{
+	return !strcmp(chip->label, data);
+}
+
+static int mv88x2242_reset_phy(struct phy_device *phydev)
+{
+	struct gpio_chip *gc;
+
+	gc = gpiochip_find("pca9555", mv88x2242_gpiochip_match_by_label);
+	if (!gc) {
+		dev_err(&phydev->dev,
+			"Can't find gpio chip with label pca9555\n");
+		return -ENODEV;
+	} else
+		dev_info(&phydev->dev,
+			"Found gpio chip with label pca9555, gpio base = %d\n",
+			gc->base);
+
+	gpio_set_value_cansleep(gc->base + 8, 0);
+	gpio_set_value_cansleep(gc->base + 9, 0);
+	gpio_set_value_cansleep(gc->base + 10, 0);
+
+	gpio_set_value_cansleep(gc->base + 8, 1);
+	gpio_set_value_cansleep(gc->base + 9, 1);
+	gpio_set_value_cansleep(gc->base + 10, 1);
+
+	return 0;
+}
+
 static int mv88x2242_config_init(struct phy_device *phydev)
 {
+	if (!mv88x2242_chip_cfg) {
+		/* chip level config */
+		mv88x2242_reset_phy(phydev);
+		mv88x2242_chip_level_config(phydev);
+		mv88x2242_chip_cfg = 1;
+	}
+
+	/* port level config */
+	mv88x2242_of_reg_init(phydev);
+
 	phydev->supported = SUPPORTED_10000baseR_FEC;
 	phydev->advertising = ADVERTISED_10000baseR_FEC;
 	phydev->state = PHY_NOLINK;
 	phydev->autoneg = AUTONEG_DISABLE;
-
-	mv88x2242_of_reg_init(phydev);
 
 	return 0;
 }
@@ -114,30 +214,30 @@ static int mv88x2242_read_status(struct phy_device *phydev)
 {
 	int rx_signal_detect;
 	int pcs_status;
-/*	int xgxs_lane_status;*/
 
 	rx_signal_detect = phy_read(phydev, MV88X2242_PMD_RX_SIGNAL_DETECT);
 	if (rx_signal_detect < 0)
 		return rx_signal_detect;
 
-	if ((rx_signal_detect & 1) == 0)
+	if ((rx_signal_detect & BIT(0)) == 0)
 		goto no_link;
 
 	pcs_status = phy_read(phydev, MV88X2242_10GBASER_PCS_STATUS);
 	if (pcs_status < 0)
 		return pcs_status;
 
-	if ((pcs_status & 1) == 0)
+	if ((pcs_status & BIT(0)) == 0)
 		goto no_link;
 
-#if 0
-	xgxs_lane_status = phy_read(phydev, MV88X2242_XGXS_LANE_STATUS);
-	if (xgxs_lane_status < 0)
-		return xgxs_lane_status;
+	pcs_status = phy_read(phydev, MV88X2242_BASER_PCS_STATUS);
+	if (pcs_status < 0)
+		return pcs_status;
 
-	if ((xgxs_lane_status & 0x1000) == 0)
+	if ((pcs_status & BIT(12)) == 0) {
+		dev_warn(&phydev->dev,
+			"WARN: host side receive link down\n");
 		goto no_link;
-#endif
+	}
 
 	phydev->speed = 10000;
 	phydev->link = 1;
@@ -191,13 +291,14 @@ static int mv88x2242_ack_interrupt(struct phy_device *phydev)
 
 static int mv88x2242_match_phy_device(struct phy_device *phydev)
 {
-	return phydev->c45_ids.device_ids[4] == MARVELL_PHY_ID_88X2242;
+	return (phydev->c45_ids.device_ids[4] & MARVELL_PHY_ID_MASK) ==
+		(MARVELL_PHY_ID_88X2242 & MARVELL_PHY_ID_MASK);
 }
 
 static struct phy_driver mv88x2242_driver[] = {
 {
 	.phy_id		= MARVELL_PHY_ID_88X2242,
-	.phy_id_mask	= 0xfffffff0,
+	.phy_id_mask	= MARVELL_PHY_ID_MASK,
 	.name		= "Marvell 88x2242",
 	.flags		= PHY_HAS_INTERRUPT,
 	.config_init	= mv88x2242_config_init,
@@ -213,6 +314,8 @@ static struct phy_driver mv88x2242_driver[] = {
 
 static int __init mv88x2242_init(void)
 {
+	mv88x2242_chip_cfg = 0;
+
 	return phy_drivers_register(mv88x2242_driver,
 		ARRAY_SIZE(mv88x2242_driver));
 }
