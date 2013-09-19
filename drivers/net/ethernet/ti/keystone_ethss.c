64001fb3349eae96371f07a7d1df3b39047e5baa
WingMan Kwok <w-kwok2@ti.com>
2013-09-19 16:47:08 -0400

    net: keystone: cpsw: ale should only be destroyed when cpsw is removed
    The ALE instance should not be destroyed in the closing the CPSW instance
    as ALE is needed for further clean up, such as removing vlan, later.
    The ALE instance should only be destoryed when the CPSW instance is removed.
    
    Signed-off-by: Jingting Zhou <zhoujt@ti.com>
    Signed-off-by: WingMan Kwok <w-kwok2@ti.com>

--- drivers/net/ethernet/ti/keystone_ethss.c
+++ drivers/net/ethernet/ti/keystone_ethss.c
@@ -2749,9 +2749,6 @@ static int cpsw_close(void *intf_priv, struct net_device *ndev)
 	
 	for_each_slave(cpsw_intf, cpsw_slave_stop, cpsw_dev);
 
-	if (!cpsw_dev->ale_refcnt)
-		cpsw_ale_destroy(cpsw_dev->ale);
-
 	netcp_unregister_txhook(netcp, CPSW_TXHOOK_ORDER, cpsw_tx_hook,
 				cpsw_intf);
 	netcp_txpipe_close(&cpsw_intf->tx_pipe);
@@ -2769,6 +2766,8 @@ static int cpsw_remove(struct netcp_device *netcp_device, void *inst_priv)
 	struct cpsw_intf *cpsw_intf, *tmp;
 
 	of_node_put(cpsw_dev->interfaces);
+
+	cpsw_ale_destroy(cpsw_dev->ale);
 
 	list_for_each_entry_safe(cpsw_intf, tmp, &cpsw_dev->cpsw_intf_head,
 				 cpsw_intf_list) {
