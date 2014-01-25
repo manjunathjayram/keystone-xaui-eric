84a3b7505abda23acfe727485ffbf0c0818cbbd6
Santosh Shilimkar <santosh.shilimkar@ti.com>
2014-01-25 18:22:54 -0500

    ARM: mm: introduce arch hooks for dma address translation routines
    Currently arch specific DMA address translation routines can be enabled
    using only defines which makes impossible to use them in with
    multi-platform builds.
    
    Hence, introduce arch specific hooks for DMA address translations
    routines to be compatible with multi-platform builds:
    dma_addr_t (*arch_pfn_to_dma)(struct device *dev, unsigned long pfn);
    unsigned long (*arch_dma_to_pfn)(struct device *dev, dma_addr_t addr);
    void* (*arch_dma_to_virt)(struct device *dev, dma_addr_t addr);
    dma_addr_t (*arch_virt_to_dma)(struct device *dev, void *addr);
    
    In case if architecture won't use it - DMA address translation routines
    will fall-back to existing implementation.
    
    Also, modify machines omap1, ks8695, iop13xx to use new DMA hooks.
    
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Nicolas Pitre <nicolas.pitre@linaro.org>
    Cc: Greg Ungerer <gerg@uclinux.org>
    Cc: Tony Lindgren <tony@atomide.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Olof Johansson <olof@lixom.net>
    Signed-off-by: Grygorii Strashko <grygorii.strashko@ti.com>
    Signed-of-by: Santosh Shilimkar <santosh.shilimkar@ti.com>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -39,6 +39,35 @@
 
 #include "mm.h"
 
+static inline dma_addr_t __pfn_to_dma(struct device *dev, unsigned long pfn)
+{
+	return (dma_addr_t)__pfn_to_bus(pfn);
+}
+
+static inline unsigned long __dma_to_pfn(struct device *dev, dma_addr_t addr)
+{
+	return __bus_to_pfn(addr);
+}
+
+static inline void *__dma_to_virt(struct device *dev, dma_addr_t addr)
+{
+	return (void *)__bus_to_virt((unsigned long)addr);
+}
+
+static inline dma_addr_t __virt_to_dma(struct device *dev, void *addr)
+{
+	return (dma_addr_t)__virt_to_bus((unsigned long)(addr));
+}
+
+dma_addr_t (*__arch_pfn_to_dma)(struct device *dev, unsigned long pfn) = __pfn_to_dma;
+EXPORT_SYMBOL(__arch_pfn_to_dma);
+unsigned long (*__arch_dma_to_pfn)(struct device *dev, dma_addr_t addr) = __dma_to_pfn;
+EXPORT_SYMBOL(__arch_dma_to_pfn);
+void* (*__arch_dma_to_virt)(struct device *dev, dma_addr_t addr) = __dma_to_virt;
+EXPORT_SYMBOL(__arch_dma_to_virt);
+dma_addr_t (*__arch_virt_to_dma)(struct device *dev, void *addr) = __virt_to_dma;
+EXPORT_SYMBOL(__arch_virt_to_dma);
+
 /*
  * The DMA API is built upon the notion of "buffer ownership".  A buffer
  * is either exclusively owned by the CPU (and therefore may be accessed
