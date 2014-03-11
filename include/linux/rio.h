39bc047cb4d9861576515f004071cd835baccaca
Aurelien Jacquiot <a-jacquiot@ti.com>
2014-03-11 18:58:15 +0100

    rapidio: rio-dev: use subsys interface for rio-dev device management
    Signed-off-by: Aurelien Jacquiot <a-jacquiot@ti.com>

--- include/linux/rio.h
+++ include/linux/rio.h
@@ -515,10 +515,6 @@ struct rio_scan_node {
 #define RIO_DBELL_TX         _IOR('R', 4, int) /* Sent a doorbell */
 #define RIO_DBELL_RX         _IOR('R', 5, int) /* Receive a doorbell */
 
-extern int rio_dev_init(void);
-extern void rio_dev_exit(void);
-extern int rio_dev_add(struct rio_dev *rdev);
-
 #endif /* CONFIG_RAPIDIO_DEV */
 
 /* Architecture and hardware-specific functions */
