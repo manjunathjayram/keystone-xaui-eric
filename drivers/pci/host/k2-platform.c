d137b32a480c9cb02a0eb32a91da613efa3d5c38
Murali Karicheri <m-karicheri2@ti.com>
2014-04-16 13:45:21 -0400

    pci: keystone: add PCIE root complex support
    TI Keystone platforms contains a PCIE module which supports
    a single bi-directional link interface with a maximum of
    two lanes width (x2).  The module supports Root Complex
    and End Point operation modes.
    
    This commit adds PCIE Root Complex driver support on the
    Keystone platforms.
    
    Following are the functions implemented:-
    
    add support of Keystone PCIe legacy irq by implementing an irq chip.
    
    history for v3.10
    
    Following bugs fixed
     1. XFER_TEST undefined. This was carried over from unit test
     2. legacy irqchip was using incorrect irq number in
        keystone_pcie_map_irq()
     3. Set irq flags for irqs
     4. fix resource handling when scanning root bus
     5. fix abort exception handling during PCI bus enumeration
        This fixes the 'unhandled fault: asynchronous external abort' message
        that may occurs during boot after PCI bus enumeration.
        Such abort exception is used to check if a EP exists during the
        bus enumeration. On KeyStone it is the exception 17, not the 8 which
        is used. Also some checks were wrong in the custom exception handler
        and this mechanism must be disabled when the PCI bus enumeration is
        finished.
        It is now unclear why such external asynchronous exception occurs so
        late during the boot and not during the PCI bus probing.
    
      6. irq updates and keystone specific quirks
        Update IRQ_EOI for msi irq ack function. Also add a pci quirks to
        change maximum read request size to 256 bytes to force all EP
        to use this size for Keystone PCI.
      7. fix INTx support and interrupt register offset
      8. add fatal error irq handling
         Currently add a error print message for fatal irq error.
         Also fix a bug in clearing existing error irqs and make
         the pr_xx statements consistent across the file
      9. move platform setup code to driver folder
         serdes initialization and RC mode setting are specific to a
         platform. This patch adds a K2 specific platform setup code
         for PCIE and move the same to the driver folder. To support
         PCI on another platform/device user may add a new match id string
         and implement another platform setup code function for the
         new device through this mechanism. If platform code is same,
         the bindings for PCI can be same as in K2.
         This patch also enable PCI link training based on DTS
         attribute "enable-linktrain" to disable link training when
         boot loader does the same.
         add a Documentation for the PCIE bindings.
     10. fix section mismatch warnings during build
     11. update pcie serdes configuration
         PCIE serdes configuration update plus some clean up.
     12. enable PCI host kernel configuration
     13. update to fix up irq for AER
         keystone PCIE Sub system uses platform IRQ for AER. This requires
         an irq fix up to update root port dev for AER error irq #12. Also
         register a shared irq handler to ack the interrupt.
         When Error gets enabled in the EP device (SERR_EN in STATUS_COMMAND
         register and and AER fatal, non fatal, correctable and unsupported
         request errors in DEV_STAT_CTRL), EP sends an error message to RC
         RC port. AER error handler is attached to the error irq line.
         Upon receiving the error, and if the error mask is cleared for the
         error, RC port trigger an IRQ event to CPU. AER driver has a work
         thread that handles the error and initiate reporting and recovery.
         See Documentation/PCI/pcieaer-howto.txt for more details on AER
         driver.
    
    Signed-off-by: Murali Karicheri <m-karicheri2@ti.com>

---
+++ drivers/pci/host/k2-platform.c
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2013 Texas Instruments, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <asm/setup.h>
+
+#include <linux/delay.h>
+#include <linux/io.h>
+#include <linux/kernel.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+
+#include "pci-pdata.h"
+
+#define reg_dump(addr, mask) \
+		pr_debug("reg %p has value %x\n", (void *)addr, \
+				(__raw_readl(addr) & ~mask))
+
+#define PCIE_RC_MODE		(BIT(2))
+#define PCIE_MODE_MASK		(BIT(1) | BIT(2))
+
+/* mask bits point to bits being modified */
+#define reg_rmw(addr, value, mask) \
+	__raw_writel(((__raw_readl(addr) & (~(mask))) | \
+			(value & (mask))), (addr))
+
+struct serdes_config {
+	u32 reg;
+	u32 val;
+	u32 mask;
+};
+
+static struct serdes_config k2_100mhz_pcie_5gbps_serdes[] = {
+	{0x0000, 0x00000800, 0x0000ff00},
+	{0x0060, 0x00041c5c, 0x00ffffff},
+	{0x0064, 0x0343c700, 0xffffff00},
+	{0x006c, 0x00000012, 0x000000ff},
+	{0x0068, 0x00070000, 0x00ff0000},
+	{0x0078, 0x0000c000, 0x0000ff00},
+
+	{0x0200, 0x00000000, 0x000000ff},
+	{0x0204, 0x5e000080, 0xff0000ff},
+	{0x0208, 0x00000006, 0x000000ff},
+	{0x0210, 0x00000023, 0x000000ff},
+	{0x0214, 0x2e003060, 0xff00ffff},
+	{0x0218, 0x76000000, 0xff000000},
+	{0x022c, 0x00200002, 0x00ff00ff},
+	{0x02a0, 0xffee0000, 0xffff0000},
+	{0x02a4, 0x0000000f, 0x000000ff},
+	{0x0204, 0x5e000000, 0xff000000},
+	{0x0208, 0x00000006, 0x000000ff},
+	{0x0278, 0x00002000, 0x0000ff00},
+	{0x0280, 0x00280028, 0x00ff00ff},
+	{0x0284, 0x2d0f0385, 0xffffffff},
+	{0x0250, 0xd0000000, 0xff000000},
+	{0x0284, 0x00000085, 0x000000ff},
+	{0x0294, 0x20000000, 0xff000000},
+
+	{0x0400, 0x00000000, 0x000000ff},
+	{0x0404, 0x5e000080, 0xff0000ff},
+	{0x0408, 0x00000006, 0x000000ff},
+	{0x0410, 0x00000023, 0x000000ff},
+	{0x0414, 0x2e003060, 0xff00ffff},
+	{0x0418, 0x76000000, 0xff000000},
+	{0x042c, 0x00200002, 0x00ff00ff},
+	{0x04a0, 0xffee0000, 0xffff0000},
+	{0x04a4, 0x0000000f, 0x000000ff},
+	{0x0404, 0x5e000000, 0xff000000},
+	{0x0408, 0x00000006, 0x000000ff},
+	{0x0478, 0x00002000, 0x0000ff00},
+	{0x0480, 0x00280028, 0x00ff00ff},
+	{0x0484, 0x2d0f0385, 0xffffffff},
+	{0x0450, 0xd0000000, 0xff000000},
+	{0x0494, 0x20000000, 0xff000000},
+
+	{0x0604, 0x00000080, 0x000000ff},
+	{0x0600, 0x00000000, 0x000000ff},
+	{0x0604, 0x5e000000, 0xff000000},
+	{0x0608, 0x00000006, 0x000000ff},
+	{0x0610, 0x00000023, 0x000000ff},
+	{0x0614, 0x2e003060, 0xff00ffff},
+	{0x0618, 0x76000000, 0xff000000},
+	{0x062c, 0x00200002, 0x00ff00ff},
+	{0x06a0, 0xffee0000, 0xffff0000},
+	{0x06a4, 0x0000000f, 0x000000ff},
+	{0x0604, 0x5e000000, 0xff000000},
+	{0x0608, 0x00000006, 0x000000ff},
+	{0x0678, 0x00002000, 0x0000ff00},
+	{0x0680, 0x00280028, 0x00ff00ff},
+	{0x0684, 0x2d0f0385, 0xffffffff},
+	{0x0650, 0xd0000000, 0xff000000},
+	{0x0694, 0x20000000, 0xff000000},
+
+	{0x0800, 0x00000000, 0x000000ff},
+	{0x0804, 0x5e000080, 0xff0000ff},
+	{0x0808, 0x00000006, 0x000000ff},
+	{0x0810, 0x00000023, 0x000000ff},
+	{0x0814, 0x2e003060, 0xff00ffff},
+	{0x0818, 0x76000000, 0xff000000},
+	{0x082c, 0x00200002, 0x00ff00ff},
+	{0x08a0, 0xffee0000, 0xffff0000},
+	{0x08a4, 0x0000000f, 0x000000ff},
+	{0x0804, 0x5e000000, 0xff000000},
+	{0x0808, 0x00000006, 0x000000ff},
+	{0x0878, 0x00002000, 0x0000ff00},
+	{0x0880, 0x00280028, 0x00ff00ff},
+	{0x0884, 0x2d0f0385, 0xffffffff},
+	{0x0850, 0xd0000000, 0xff000000},
+	{0x0894, 0x20000000, 0xff000000},
+
+	{0x0a00, 0x00000100, 0x0000ff00},
+	{0x0a08, 0x00e12c08, 0x00ffffff},
+	{0x0a0c, 0x00000081, 0x000000ff},
+	{0x0a18, 0x00e80000, 0x00ff0000},
+	{0x0a30, 0x002f2f00, 0x00ffff00},
+	{0x0a4c, 0xac820000, 0xffff0000},
+	{0x0a54, 0xc0000000, 0xff000000},
+	{0x0a58, 0x00001441, 0x0000ffff},
+	{0x0a84, 0x00000301, 0x0000ffff},
+
+	{0x0a8c, 0x81030000, 0xffff0000},
+	{0x0a90, 0x00006001, 0x0000ffff},
+	{0x0a94, 0x01000000, 0xff000000},
+	{0x0aa0, 0x81000000, 0xff000000},
+	{0x0abc, 0xff000000, 0xff000000},
+	{0x0ac0, 0x0000008b, 0x000000ff},
+
+	{0x0000, 0x00000003, 0x000000ff},
+	{0x0a00, 0x0000009f, 0x000000ff},
+
+	{0x0a44, 0x5f733d00, 0xffffff00},
+	{0x0a48, 0x00fdca00, 0x00ffff00},
+	{0x0a5c, 0x00000000, 0xffff0000},
+	{0x0a60, 0x00008000, 0xffffffff},
+	{0x0a64, 0x0c581220, 0xffffffff},
+	{0x0a68, 0xe13b0602, 0xffffffff},
+	{0x0a6c, 0xb8074cc1, 0xffffffff},
+	{0x0a70, 0x3f02e989, 0xffffffff},
+	{0x0a74, 0x00000001, 0x000000ff},
+	{0x0b14, 0x00370000, 0x00ff0000},
+	{0x0b10, 0x37000000, 0xff000000},
+	{0x0b14, 0x0000005d, 0x000000ff},
+};
+
+int k2_pcie_platform_setup(void *pdata, struct device_node *np)
+{
+	struct keystone_pcie_pdata *p_data = pdata;
+	void __iomem *reg_serdes_base, *devcfg;
+	struct serdes_config *p;
+	u32 val;
+	int i;
+
+	devcfg = of_iomap(np, 1);
+	reg_serdes_base = of_iomap(np, 2);
+
+	pr_info("keystone2_pcie_serdes_setup\n");
+
+	if (!reg_serdes_base)
+		pr_info("Assuming SERDES initialized by boot loader\n");
+
+	if (!devcfg) {
+		pr_warn("pcie device cfg bindings missing\n");
+		return -EINVAL;
+	}
+
+	if (reg_serdes_base) {
+		for (i = 0, p = &k2_100mhz_pcie_5gbps_serdes[0];
+			i < ARRAY_SIZE(k2_100mhz_pcie_5gbps_serdes);
+			i++, p++) {
+			reg_rmw((reg_serdes_base + p->reg), p->val, p->mask);
+			reg_dump((reg_serdes_base + p->reg), p->mask);
+		}
+	}
+
+	udelay(2000);
+
+	/* enable RC mode in devcfg */
+	val = __raw_readl(devcfg);
+	val &= ~PCIE_MODE_MASK;
+	val |= PCIE_RC_MODE;
+	__raw_writel(val, devcfg);
+
+	/* check if we need to enable link training */
+	p_data->en_link_train =
+		(of_get_property(np, "enable-linktrain", NULL) != NULL);
+
+	pr_info("keystone2_pcie_serdes_setup done, en_link_train = %d\n",
+		p_data->en_link_train);
+	return 0;
+}
