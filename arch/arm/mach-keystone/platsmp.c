6ffa5ae8bb758b34007301b905bda2ce35138b5b
Murali Karicheri <m-karicheri2@ti.com>
2014-07-24 11:35:20 -0400

    ARM: keystone: remove hardcoding of number of possible CPUs
    Currently on K2L boot up log shows
    
    [  641.824274] CPU2: failed to boot: -2
    [  641.824406] CPU3: failed to boot: -2
    
    This is because keystone_smp_init_cpus() calls set_cpu_possible()
    for N_CPUS which is set to 4, the maximum possible CPUs. nr_cpu_ids
    gets initialized with number of available CPUs based on DT nodes and
    there is no need for implementing smp_init_cpus(). Also remove
    smp_prepare_cpus init code as it is not doing anything. This is in
    sync with latest upstream code.
    
    Signed-off-by: Murali Karicheri <m-karicheri2@ti.com>

--- arch/arm/mach-keystone/platsmp.c
+++ arch/arm/mach-keystone/platsmp.c
@@ -29,27 +29,6 @@
 
 #include "keystone.h"
 
-static void __init keystone_smp_init_cpus(void)
-{
-	unsigned int i, ncores;
-
-	ncores = 4;
-
-	/* sanity check */
-	if (ncores > NR_CPUS) {
-		pr_warn("restricted to %d cpus\n", NR_CPUS);
-		ncores = NR_CPUS;
-	}
-
-	for (i = 0; i < ncores; i++)
-		set_cpu_possible(i, true);
-}
-
-static void __init keystone_smp_prepare_cpus(unsigned int max_cpus)
-{
-	/* nothing to do here */
-}
-
 static void __cpuinit keystone_smp_secondary_initmem(void)
 {
 #ifdef CONFIG_ARM_LPAE
@@ -106,8 +85,6 @@ static int keystone_cpu_disable(unsigned int cpu)
 #endif
 
 struct smp_operations keystone_smp_ops __initdata = {
-	.smp_init_cpus		= keystone_smp_init_cpus,
-	.smp_prepare_cpus	= keystone_smp_prepare_cpus,
 	.smp_secondary_init	= keystone_smp_secondary_init,
 	.smp_boot_secondary	= keystone_smp_boot_secondary,
 #ifdef CONFIG_HOTPLUG_CPU
