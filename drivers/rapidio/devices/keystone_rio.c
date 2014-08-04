3c11de5c0be238418c2818542612c35565a954b6
Aurelien Jacquiot <a-jacquiot@ti.com>
2014-08-04 13:27:00 -0400

    rapidio: keystone: fix kernel link warning (wrong section usage)
    The KeyStone RapidIO driver was early started on a 2.6.34 kernel
    where probe functions from platform_driver definition must be
    defined with __devinit or __init qualifiers.
    With recent kernel 3.x this has been removed for generic
    hot-plug support purpose (probe are now standard static functions)
    but the KeyStone RapidIO driver was not modified in that sense.
    As a consequence a section mismatch warning was displayed during
    the Linux kernel image link for the keystone_rio_probe() function.
    
    Signed-off-by: Aurelien Jacquiot <a-jacquiot@ti.com>

--- drivers/rapidio/devices/keystone_rio.c
+++ drivers/rapidio/devices/keystone_rio.c
@@ -3682,7 +3682,7 @@ out:
 	return res;
 }
 
-static int __init keystone_rio_probe(struct platform_device *pdev)
+static int keystone_rio_probe(struct platform_device *pdev)
 {
 	struct device_node *node = pdev->dev.of_node;
 	struct keystone_rio_data *krio_priv;
