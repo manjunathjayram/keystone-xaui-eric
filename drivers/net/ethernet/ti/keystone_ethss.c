efd468cfeeb35dbb843540d6bcc8c61231a054a7
WingMan Kwok <w-kwok2@ti.com>
2015-02-27 16:18:33 -0500

    net: keystone: add support of additional 1G sgmii mode configurations
    This patch adds support of 3 1G sgmii mode configurations
    1. MAC-to-PHY master (SGMII_LINK_MAC_PHY_MASTER)
    2. MAC-to-PHY master NO MDIO (SGMII_LINK_MAC_PHY_MASTER_NO_MDIO)
            - SGMII is in master mode and auto-nego enabled
            - use this mode to work around link down/up
              issue observed on K2E/L.
            - these 2 modes supports only gigabit fullduplex connections.
    
    3. MAC-to-MAC Auto-negotiation Slave (SGMII_LINK_MAC_MAC_AN_SLAVE)
            - When connected 2 EVMs back-to-back and one
              side is configured as MAC-to-MAC Auto-negotiation master
              the other side needs to be configured as auto-nego slave.
    
    Signed-off-by: WingMan Kwok <w-kwok2@ti.com>

--- drivers/net/ethernet/ti/keystone_ethss.c
+++ drivers/net/ethernet/ti/keystone_ethss.c
@@ -161,6 +161,9 @@
 #define SGMII_BASE(s) \
 	(((s) < 2) ? cpsw_dev->sgmii_port_regs : cpsw_dev->sgmii_port34_regs)
 
+#define IS_SGMII_MAC_PHY(i) \
+	(((i) == SGMII_LINK_MAC_PHY) || ((i) == SGMII_LINK_MAC_PHY_MASTER))
+
 /* CPSW SERDES */
 #define CPSW_SERDES_MAX_NUM		1
 #define CPSW_LANE_NUM_PER_SERDES	4
@@ -2223,7 +2226,7 @@ static void cpsw_slave_link(struct cpsw_slave *slave,
 	struct netcp_priv *netcp = netdev_priv(cpsw_intf->ndev);
 	int sn = slave->slave_num;
 
-	if (slave->link_interface == SGMII_LINK_MAC_PHY) {
+	if (IS_SGMII_MAC_PHY(slave->link_interface)) {
 		/* check only the bit in phy_link_state_mask
 		 * that corresponds to the slave
 		 */
@@ -2271,7 +2274,7 @@ static void cpsw_slave_open(struct cpsw_slave *slave,
 	cpsw_ale_add_mcast(priv->ale, cpsw_intf->ndev->broadcast,
 			   1 << slave_port, 0, 0, ALE_MCAST_FWD_2);
 
-	if (slave->link_interface == SGMII_LINK_MAC_PHY) {
+	if (IS_SGMII_MAC_PHY(slave->link_interface)) {
 		slave->phy = of_phy_connect(cpsw_intf->ndev,
 					    cpsw_intf->phy_node,
 					    &cpsw_adjust_link, 0,
@@ -2683,7 +2686,7 @@ static void cpsw_timer(unsigned long arg)
 	 */
 	if (!cpsw_dev->multi_if ||
 	    (cpsw_dev->multi_if &&
-	     cpsw_intf->slaves->link_interface != SGMII_LINK_MAC_PHY)) {
+	     !IS_SGMII_MAC_PHY(cpsw_intf->slaves->link_interface))) {
 		if (cpsw_intf->link_state)
 			netif_carrier_on(cpsw_intf->ndev);
 		else
